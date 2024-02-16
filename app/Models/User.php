73b058f15d42a99220d8018e436c94a856f9eb36
bastianNebra <123bastianlontsi@gmail.com>
2024-02-16 14:36:22 +0100

    Setting up the backend

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,16 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+
+    public function driver(){
+        return $this->hasOne(Driver::class);
+    }
+
+    public function trips(){
+        return $this->hasMany(Trip::class);
+    }
+
+    
+
+
 }
